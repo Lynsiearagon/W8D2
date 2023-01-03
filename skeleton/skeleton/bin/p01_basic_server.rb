4dc60005b93193c5935c9df9ccbce1cebaf19f54
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-01-03 16:17:45 -0500

    Rails lite Phase 1 complete, Phase 2 started

---
+++ skeleton/skeleton/bin/p01_basic_server.rb
@@ -0,0 +1,25 @@
+require 'rack'
+
+
+# example = Proc.new do |evn|
+#     [
+#         200,
+#         {'Content-Type' => 'text/html'},
+#         ['hello world']
+#     ]
+# end
+
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    res.write("Hello world!")
+    res.finish
+end
+
+
+Rack::Server.start(
+    app: app,
+    Port: 3000
+)
